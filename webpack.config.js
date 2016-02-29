455170a083458477b004a6ef78ec3e4fbbed7091
ke-pan <ke_pan@yeah.net>
2016-02-29 21:24:16 +0800

    some config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const Path = require("path")
 
 module.exports = {
-    entry: Path.resolve(__dirname, "./src/index.js"),
-    output: {
-        path: Path.resolve(__dirname, "./build"),
-        filename: "bundle.js",
-        publicPath: "./bundle"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                include: Path.resolve(__dirname, "./src"),
-                loader: "babel",
-                query: {
-                    presets: ["react", "es2015"]
-                }
-            },
-            {
-                test: /\.css$/,
-                include: Path.resolve(__dirname, "./src"),
-                loader: "style-loader!css-loader"
-            }
-        ]
-    }
+  entry: Path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: Path.resolve(__dirname, "./build"),
+    filename: "bundle.js",
+    publicPath: "./bundle"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: Path.resolve(__dirname, "./src"),
+        loader: "babel",
+        query: {
+          presets: ["react", "es2015"]
+        }
+      },
+      {
+        test: /\.css$/,
+        include: Path.resolve(__dirname, "./src"),
+        loader: "style-loader!css-loader"
+      }
+    ]
+  }
 }
