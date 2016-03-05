52862a51f569573fda028a1e03e8fd2dc4608ee6
ke-pan <ke_pan@yeah.net>
2016-03-05 18:05:34 +0800

    app bar

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './app.css';
 import { Link } from 'react-router';
+import AppBar from 'material-ui/lib/app-bar';
+import RaisedButton from 'material-ui/lib/raised-button'
+
+const buttonStyle = {
+  margin: 12,
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="app">
-        This is landing page
-        <ul role="nav">
-          <li><Link to="login">Log in</Link></li>
-          <li><Link to="signup">Sign up</Link></li>
-        </ul>
+        <AppBar
+          title="Meetie"
+          showMenuIconButton={false}
+        >
+          <Link to="login">
+            <RaisedButton label="Log in" default={true} style={buttonStyle} />
+          </Link>
+          <Link to="signup">
+            <RaisedButton label="Sign up" primary={true} style={buttonStyle}/>
+          </Link>
+        </AppBar>
+
+        {this.props.children}
       </div>
     );
   }
