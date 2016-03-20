8664180e212359c5f69c0df1af5bd7e3d4f2647f
ke-pan <ke_pan@yeah.net>
2016-03-20 16:19:22 +0800

    chore: Reducer hot reload

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ injectTapEventPlugin();
 
 const store = createStore(reducers);
 
+if (module.hot) {
+  // Enable Webpack hot module replacement for reducers
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
+
 console.log(store.getState());
 
 const history = syncHistoryWithStore(browserHistory, store)
